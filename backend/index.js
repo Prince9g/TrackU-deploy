1a716cc4cf1e55f9b211f3affa6fcbec8b0ff91a
Prince9g <142648202+Prince9g@users.noreply.github.com>
2024-12-27 21:38:04 +0530

    added dist folder for run build command

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,40 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
 import connectDB from './utils/db.js';
-import bodyParser from 'body-parser';
-
+// import bodyParser from 'body-parser';
+import path from "path";
 
 const { urlencoded } = express;
 const app = express();
 dotenv.config({});
 // app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    return res.status(200).json({
-        message: 'yes!!',
-        success: true
-    });
-});
+const _dirname = path.resolve();
+// app.get('/', (req, res) => {
+//     return res.status(200).json({
+//         message: 'yes!!',
+//         success: true
+//     });
+// });
 
 // middlewares
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-};
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //apis will fetch here
 app.use('/user', userRoute);
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res)=> {
+    res.sendFile(path.resolve(_dirname,"frontend", "dist", "index.html"));
+})
 const PORT = 8080;
 app.listen(PORT, () => {
     connectDB();
